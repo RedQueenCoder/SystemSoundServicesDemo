44eface138ceeed4fe4583a9b7930178d347a7c3
Janie Clayton-Hasz <janie.k.clayton@gmail.com>
2014-03-07 16:25:36 -0600

    committing the changes to the audio session categories

--- SnowMobileDemo/AppDelegate.m
+++ SnowMobileDemo/AppDelegate.m
@@ -7,12 +7,32 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    // Insantiate the AVAudioSession singleton
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    
+    NSError *error;
+    
+//    if (![session setCategory:AVAudioSessionCategoryAmbient error:&error]) {
+//        NSLog(@"Category Error: %@", [error localizedDescription]);
+//    }
+    
+    if (![session setCategory:AVAudioSessionCategorySoloAmbient error:&error]) {
+        NSLog(@"Category Error: %@", [error localizedDescription]);
+    }
+    
+    if (![session setActive:YES error:&error]) {
+        NSLog(@"Activation Error: %@", [error localizedDescription]);
+    }
+    
     return YES;
 }
 							
--- SnowMobileDemo/ViewController.m
+++ SnowMobileDemo/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    SystemSoundID _soundID;
+    NSString *_fileName;
+}
+
+
+@property (weak, nonatomic) IBOutlet UIButton *pressMe;
+@property (weak, nonatomic) IBOutlet UILabel *tadaLabel;
+@property (nonatomic, getter = isPlaying) BOOL playing;
+- (IBAction)pressMeButton:(id)sender;
 
 @end
 
@@ -18,6 +27,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +36,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void)playSound
+{
+    _fileName = [[NSBundle mainBundle] pathForResource:@"Bubbles"
+                                                ofType:@"caf"];
+    assert(nil != _fileName);
+    
+    NSURL *url = [NSURL fileURLWithPath:_fileName];
+    assert(nil != url);
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef) url, &_soundID);
+    AudioServicesPlaySystemSound(_soundID);
+}
+
+- (IBAction)pressMeButton:(id)sender {
+    // Change the label text
+    self.tadaLabel.text = @"Ta Da!! :D";
+    
+    [self playSound];
+}
+
+- (void) dealloc
+{
+    AudioServicesDisposeSystemSoundID(_soundID);
+}
 @end
